8cc4f66231260bf21f0e6ce6b803fff92c578b45
youngjinc <cyjin6789@gmail.com>
2022-05-17 11:55:01 +0900

    [REFACTOR] 코드 정리

--- app/src/main/java/org/sopt/jointseminar/melon/presentation/MainActivity.kt
+++ app/src/main/java/org/sopt/jointseminar/melon/presentation/MainActivity.kt
@@ -1,12 +1,14 @@
 package org.sopt.jointseminar.melon.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import org.sopt.jointseminar.melon.R
 import org.sopt.jointseminar.melon.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -15,7 +17,7 @@ class MainActivity : AppCompatActivity() {
         initBottomNavi()
     }
 
-    private fun initBottomNavi(){
+    private fun initBottomNavi() {
         val homeFragment = HomeFragment()
         val chartFragment = ChartFragment()
         val liveFragment = LiveFragment()
@@ -23,35 +25,21 @@ class MainActivity : AppCompatActivity() {
         val storageFragment = StorageFragment()
 
         supportFragmentManager.beginTransaction().add(R.id.fcv_main, homeFragment).commit()
-        binding.bnvMain.setOnItemSelectedListener{
+        binding.bnvMain.setOnItemSelectedListener {
             val transaction = supportFragmentManager.beginTransaction()
-            when(it.itemId){
-                R.id.menu_home->{
-                    transaction.replace(R.id.fcv_main,homeFragment)
-                    transaction.commit()
-                    return@setOnItemSelectedListener true
-                }
-                R.id.menu_chart->{
-                    transaction.replace(R.id.fcv_main,chartFragment)
-                    transaction.commit()
-                    return@setOnItemSelectedListener true
-                }
-                R.id.menu_live->{
-                    transaction.replace(R.id.fcv_main,liveFragment)
-                    transaction.commit()
-                    return@setOnItemSelectedListener true
-                }
-                R.id.menu_search->{
-                    transaction.replace(R.id.fcv_main,searchFragment)
-                    transaction.commit()
-                    return@setOnItemSelectedListener true
-                }
-                else->{
-                    transaction.replace(R.id.fcv_main,storageFragment)
-                    transaction.commit()
-                    return@setOnItemSelectedListener true
-                }
+            when (it.itemId) {
+                R.id.menu_home -> replaceFragment(transaction, homeFragment)
+                R.id.menu_chart -> replaceFragment(transaction, chartFragment)
+                R.id.menu_live -> replaceFragment(transaction, liveFragment)
+                R.id.menu_search -> replaceFragment(transaction, searchFragment)
+                else -> replaceFragment(transaction, storageFragment)
             }
+            return@setOnItemSelectedListener true
         }
+    }
+
+    private fun replaceFragment(transaction: FragmentTransaction, fragment: Fragment) {
+        transaction.replace(R.id.fcv_main, fragment)
+        transaction.commit()
     }
 }
