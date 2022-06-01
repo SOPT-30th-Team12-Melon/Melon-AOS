053ef70538abf7937afe5a03bec23ffce7c41217
youngjinc <cyjin6789@gmail.com>
2022-06-01 18:48:43 +0900

    [DELETE] 사용되지 않는 함수 삭제

--- app/src/main/java/org/sopt/jointseminar/melon/util/DateFormatUtil.kt
+++ app/src/main/java/org/sopt/jointseminar/melon/util/DateFormatUtil.kt
@@ -4,19 +4,6 @@ import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
 
-/** yyyy-MM-dd'T'HH:mm:ss.SSS'Z' -> long 타입으로 시간 포맷 변경 */
-fun convertTimeZonToLong(strDate: String): Long? {
-    val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
-    val date: Date?
-    try {
-        date = dateFormat.parse(strDate)
-    } catch (e: ParseException) {
-        e.printStackTrace()
-        return null
-    }
-    return date.time
-}
-
 /** yyyy-MM-dd'T'HH:mm:ss.SSS'Z' -> yyyy.MM.dd 타입으로 시간 포맷 변경 */
 fun convertTimeZonToYMD(date: String?): String? {
     val inputDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
